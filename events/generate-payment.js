c5116489f46422cb328db5888da304f9be8b844c
Daniel-D3V <danield3v@icloud.com>
2025-09-05 00:17:39 -0300

    save

--- events/generate-payment.js
+++ events/generate-payment.js
@@ -80,6 +80,7 @@ module.exports = {
                 payer: checkout.payer,
                 paymentMethod: checkout.paymentMethod,
                 referenceCode: null,
+                couponCode: checkout.coupon.code,
                 utmParams: null,
             })
                 .then((response) => {
